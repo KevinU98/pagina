90443dbd00fde6f521dddfb2083f4257c034665c
KevinU98 <59625859+KevinU98@users.noreply.github.com>
2020-11-24 13:00:33 -0600

    Envio de Correos

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,49 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+   // required headers
+   header("Access-Control-Allow-Origin: *");
+   header("Content-Type: application/json; charset=UTF-8");
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+   $nombre = $_POST['name'];
+   $email = $_POST['email'];
+   $subject = $_POST['subject'];
+   $message = $_POST['message'];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+   require '../assets/plugins/phpMailer/src/Exception.php';
+   require '../assets/plugins/phpMailer/src/PHPMailer.php';
+   require '../assets/plugins/phpMailer/src/SMTP.php';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    //Luego tenemos que iniciar la validación por SMTP:
+    $mail = new PHPMailer\PHPMailer\PHPMailer(TRUE);   
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
+    $mail->SMTPAutoTLS = false;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->Username = "kevin.uriegas@tibs.com.mx"; // GMAIL username
+    $mail->Password = "kevinalexis98"; // GMAIL password
+    // check if more than 0 record found
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    $mail->AddAddress($email); // Esta es la dirección a donde enviamos
 
-  echo $contact->send();
+    //Con estas pocas líneas iniciamos una conexión con el SMTP. Lo que ahora deberíamos hacer, es configurar el mensaje a enviar, el //From, etc.
+    $mail->From = "kevin.uriegas@tibs.com.mx"; // Desde donde enviamos (Para mostrar)
+    $mail->FromName = "Magni-Acero";
+
+    //Estas dos líneas, cumplirían la función de encabezado (En mail() usado de esta forma: “From: Nombre <correo@dominio.com>”) de //correo.
+
+    $mail->IsHTML(true); // El correo se envía como HTML
+    $mail->Subject = $subject; // Este es el titulo del email.
+    $body = '<p>Nombre: <b>'.$nombre.'</b></p>
+             <p>Email: <b>'.$email.'</b></p>
+             <p>Ademas Incluyó el siguiente mensaje:</p>
+             <p><b>'.$message.'</b></p>';
+    $mail->Body = $body; // Mensaje a enviar
+    // $exito = $mail->Send(); // Envía el correo.
+    if(!$mail->Send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        echo "OK";
+    }
 ?>
